abd53778de13ca49a016ec7784e09c85ec77cb7d
JulesFert <jfert.lyon@gmail.com>
2024-04-30 13:38:32 +0200

    portfolio WIP

--- app.js
+++ app.js
@@ -2,28 +2,52 @@ let app = {
 
     init: function() {
         console.log('Initialisation');
-
-        // je met l'écouteur d'évènement sur les images projets
-        const projects = document.querySelectorAll('#list-projects li')
-        projects.forEach(projet => {
-            projet.addEventListener("mouseenter", app.handleMouse)
-            projet.addEventListener("mouseleave", app.handleMouse)
-        });
     },
-
-    handleMouse: function(e) {
-        console.log('on passe la souris sur une image')
-        const target = e.target.children[1];
-        target.classList.toggle('default')
-
-    }
-
-
-
-
-
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    app.init()
-  });
+document.addEventListener('DOMContentLoaded', (event) => {
+    setTimeout(function() {
+        let img = document.getElementById('profile-picture')
+        img.style.opacity = 1
+    }, 500); 
+
+    setTimeout(function() {
+        let title = document.getElementById('first_title')
+        title.style.opacity = 1
+    }, 700);
+
+    setTimeout(function() {
+        let about = document.getElementById('about')
+        let competences = document.getElementById('competences')
+        let projects = document.getElementById('projects')
+        let footer = document.getElementById('contact')
+        let description = document.getElementById('description')
+        let pictures = document.getElementById('pictures')
+        let technos = document.getElementById('technos')
+        let github = document.getElementById('github')
+        if (about) {
+            about.style.opacity = 1
+        }
+        if (competences) {
+            competences.style.opacity = 1
+        }
+        if (projects) {
+            projects.style.opacity = 1
+        }
+        if (footer) {
+            footer.style.opacity = 1
+        }
+        if (description) {
+            description.style.opacity = 1
+        }
+        if(pictures) {
+            pictures.style.opacity = 1
+        }
+        if (technos) {
+            technos.style.opacity = 1
+        }
+        if (github) {
+            github.style.opacity = 1
+        }
+    }, 1200);
+});
